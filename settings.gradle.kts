b7780f0bf8ebb33d2be07b6421abceae8bab603c
EvgenNaumov <naumovev@mail.ru>
2023-09-08 11:42:47 +0300

    Start. Настройка модулей и зависимостей

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "SpeechToText"
+include (":app")
+include(":utils")
+include(":repository")
+include(":core")
+include(":model")
